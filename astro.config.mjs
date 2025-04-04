c4330bf4af92f37ae28b7974f80c1398175073d1
Eze Martinez <ezemartinezperkapita@gmail.com>
2025-04-04 10:40:47 -0300

    Feat GA4, TAG

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://innova-webdev.com",
   integrations: [tailwind(), icon()],
   server: {
     host: '0.0.0.0', // Asegúrate de que el servidor escuche en todas las interfaces
