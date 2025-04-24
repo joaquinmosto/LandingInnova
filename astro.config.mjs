e5d866dbb7ef1fd3a03901fab799831bc91417c7
Eze Martinez <ezemartinezperkapita@gmail.com>
2025-04-24 12:40:54 -0300

    Feat FAQ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import { fileURLToPath } from 'url';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://innova-webdev.com",
   integrations: [tailwind(), icon()],
   server: {
-    host: '0.0.0.0', // Asegúrate de que el servidor escuche en todas las interfaces
+    host: '0.0.0.0',
     port: 10000
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '~': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
   }
 });
